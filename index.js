9bb8e47656002521c3d473ff2a8dadd5eebd7834
sasmith_reddy <sasmith.reddy5@gmail.com>
2024-01-26 23:21:59 +0530

    Parakram 2024 Initial Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express()
+
+app.use(express.static(__dirname + '/public/'));
+app.set('view engine', 'ejs');
+
+//routes
+app.use((req,res)=>{
+    res.render('home.ejs');
+})
+
+let port = 3000 || process.env.PORT;
+app.listen(port, (req, res) => {
+    console.log(`The server is running on the port ${port}`);
+})
