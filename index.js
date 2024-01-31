3e6521e71429bc448bf7b8966ccc5b1cb0ae44ba
sasmith123 <76240250+sasmith123@users.noreply.github.com>
2024-01-31 22:13:45 +0530

    Parakram 2024!!!

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express()
 
 app.use(express.static(__dirname + '/public/'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 //routes 
